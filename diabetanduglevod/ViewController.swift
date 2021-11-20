fbda6700652ba37d26243ea73e271c730c920e26
alexey <alexeyweb60@gmail.com>
2021-11-20 23:24:19 +0200

    Initial Commit

---
+++ diabetanduglevod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diabetanduglevod
+//
+//  Created by alexey on 20.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
